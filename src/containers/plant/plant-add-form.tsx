4c6a8c19664ad30bf79cae1eaff86cab766c17a7
Aurélien St <aurelien.strimelle@bstorm.be>
2025-03-25 10:56:49 +0100

    Form to add plant

--- src/containers/plant/plant-add-form.tsx
+++ src/containers/plant/plant-add-form.tsx
@@ -1,20 +1,42 @@
 import { useDispatch } from "react-redux";
 import type { PlantData } from "../../@types/plant";
 import { plantAdd } from "../../store/plant/plant.action.ts";
+import { useActionState, useId } from "react";
+import z from 'zod';
 
+const plantAddSchema = z.object({
+    name: z.string()
+        .min(2)
+        .trim(),
+    location: z.string()
+        .trim()
+});
+
+type PlantStateForm = {
+    error: {
+        name?: string[],
+        location?: string[],
+    } | null;
+};
 
 export default function PlantAddForm() {
 
+    const inputId = useId();
     const dispatch = useDispatch();
 
-    const handleAdd = (data: FormData) => {
-
-        // TODO Créer un formulaire pour obtenir les données
-        const plant: PlantData = {
-            name: 'Rose',
-            location: 'Salon',
-            isGiven: false
+    const handleAdd = (state: PlantStateForm, data: FormData): PlantStateForm => {
+  
+        const result = plantAddSchema.safeParse(Object.fromEntries(data.entries()));
+        if (result.error) {
+            return {
+                error: result.error.flatten().fieldErrors
+            };
         }
+
+        const plant: PlantData = {
+            ...result.data,
+            isGiven: false
+        };
 
         //! Interaction avec le store pour injecter les données
         //? Utiliser l'action créator pour générer un objet "action"
@@ -24,16 +46,38 @@ export default function PlantAddForm() {
 
         //? Vous pouvez le faire en une seul ligne aussi :p
         //  dispatch(plantAdd(plant));
-    }
+
+        return {
+            error: null
+        };
+    };
+
+    const [plantFormState, handleAddAction] = useActionState(handleAdd, { error: null });
 
     return (
-        <form action={handleAdd}>
-            <div>
-                <span>Pas d'input pour simplifier la démo :o</span>
+        <form action={handleAddAction}>
+            <div className="mb-0.5">
+                <label htmlFor={inputId + '-name'}>Nom</label>
+                <input type="text" name="name" id={inputId + '-name'} />
+                {plantFormState.error?.name && (
+                    <span className="text-pink-700 font-bold pl-2">
+                        {plantFormState.error?.name.join(', ')}
+                    </span>
+                )}
+            </div>
+            <div className="mb-0.5">
+                <label htmlFor={inputId + '-location'}>Emplacement</label>
+                <input type="text" name="location" id={inputId + '-location'} />
+                {plantFormState.error?.location && (
+                    <span className="text-pink-700 font-bold pl-2">
+                        {plantFormState.error?.location.join(', ')}
+                    </span>
+                )}
             </div>
             <div>
                 <button type="submit">Ajouter</button>
             </div>
+
         </form>
-    )
+    );
 }
