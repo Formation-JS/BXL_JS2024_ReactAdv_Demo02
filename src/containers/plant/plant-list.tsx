4cec07d4018301e29cbe4b9ab5f04d18ad85dba2
Aurélien St <aurelien.strimelle@bstorm.be>
2025-03-25 13:18:20 +0100

    Exo - Dispatch action "Remove"

--- src/containers/plant/plant-list.tsx
+++ src/containers/plant/plant-list.tsx
@@ -1,16 +1,24 @@
-import { useSelector } from "react-redux";
-import type { RootState } from "../../store/store.ts";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "../../store/store.ts";
 import type { Plant } from "../../@types/plant";
 import clsx from 'clsx';
+import { plantRemove } from "../../store/plant/plant.action.ts";
 
 //? Le typage des props du composant "PlantListItem" (basé sur le type "Plant")
 type PlantListItemProps = Plant & {
-    onDemo: () => void;
+    onDemo?: () => void;
 };
 function PlantListItem({ id, name, location, isGiven }: PlantListItemProps) {
+
+    const dispatch = useDispatch<AppDispatch>();
+
+    const handleRemove = () => {
+        dispatch(plantRemove(id));
+    } 
+
     return (
-        <article className={clsx(isGiven && 'text-gray-400 line-through')}>
-            {name} ({location})
+        <article className={clsx('mb-1' ,isGiven && 'text-gray-400 line-through')}>
+            {name} ({location}) <span className="cursor-pointer" onClick={handleRemove}>🗑️</span>
         </article>
     );
 }
